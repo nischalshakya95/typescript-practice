3f0a2656015312b7d9c882fd310b8871c106bc5b
nischalshakya95 <nischalshakya@lftechnology.com>
2019-12-04 16:27:54 +0545

    Add testcase to alphanumeric sorting

--- src/codewars/alpha-numeric-sorting/alpha-numeric-sorting.spec.ts
+++ src/codewars/alpha-numeric-sorting/alpha-numeric-sorting.spec.ts
@@ -1,6 +1,6 @@
 import {AlphaNumericSorting} from "./alpha-numeric-sorting";
 
-test('Maximum between two numbers', () => {
+test('Sort alphanumeric words in ascending order on basics of number', () => {
     let alphaNumericSorting = new AlphaNumericSorting();
     expect(alphaNumericSorting.sort('is21 Thi1s T4est 3a')).toBe('Thi1s 3a T4est is21');
     expect(alphaNumericSorting.sort('4of Fo1r pe6ople g3ood th5e the2')).toBe('Fo1r the2 g3ood 4of th5e pe6ople');
