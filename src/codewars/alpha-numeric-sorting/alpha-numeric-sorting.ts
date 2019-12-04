85b2ac9fba0beb64b051ba10d2563f34c03401ce
nischalshakya95 <nischalshakya@lftechnology.com>
2019-12-04 16:29:49 +0545

    Add return type in sort method

--- src/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
+++ src/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
@@ -1,5 +1,5 @@
 export class AlphaNumericSorting {
-    sort(sentence: string) {
+    sort(sentence: string): string {
         return sentence.split(' ')
             .sort((strOne, strTwo) => {
                 return +strOne.match(/\d+/g)![0] - +strTwo.match(/\d+/g)![0];
