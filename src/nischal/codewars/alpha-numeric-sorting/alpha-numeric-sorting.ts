cce5c8441d86055bd0d3099240f3165a3bea71d5
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2019-12-05 14:21:04 +0545

    dfsf

--- src/nischal/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
+++ src/nischal/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
@@ -2,7 +2,7 @@ export class AlphaNumericSorting {
     sort(sentence: string): string {
         let newy = "sentence";
         console.log(newy);
-        return sentence.split(' ')
+        return newy.split(' ')
             .sort((strOne, strTwo) => {
                 return +strOne.match(/\d+/g)![0] - +strTwo.match(/\d+/g)![0];
             }).join(' ');
