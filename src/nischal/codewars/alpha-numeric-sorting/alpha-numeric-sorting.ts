5af7a25b45d2a110ba78f82a059f211030569724
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2019-12-05 14:14:55 +0545

    some

--- src/nischal/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
+++ src/nischal/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
@@ -1,6 +1,7 @@
 export class AlphaNumericSorting {
     sort(sentence: string): string {
-        sentence = sentence;
+        let newy = "sentence";
+        console.log(newy);
         return sentence.split(' ')
             .sort((strOne, strTwo) => {
                 return +strOne.match(/\d+/g)![0] - +strTwo.match(/\d+/g)![0];
