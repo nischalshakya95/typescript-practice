f6909149d86f99aa029515dcb77e9650ca6ec1b0
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2019-12-05 14:12:22 +0545

    conflict test

--- src/nischal/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
+++ src/nischal/codewars/alpha-numeric-sorting/alpha-numeric-sorting.ts
@@ -1,5 +1,6 @@
 export class AlphaNumericSorting {
     sort(sentence: string): string {
+        sentence = sentence;
         return sentence.split(' ')
             .sort((strOne, strTwo) => {
                 return +strOne.match(/\d+/g)![0] - +strTwo.match(/\d+/g)![0];
